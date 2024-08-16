301dd4ceb932755abbbcaa6b15c6371d2dd54043
Aria Fallah <ariaf@meta.com>
2024-08-15 21:13:01 -0400

    target="_blank" on external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import markdownIntegration from '@astropub/md';
 
 export default defineConfig({
   markdown: {
+    remarkPlugins: [remarkExternalLinks],
     shikiConfig: {
       themes: {
         light: 'github-light',
@@ -18,3 +19,26 @@ export default defineConfig({
     prefetchAll: true,
   },
 });
+
+function remarkExternalLinks() {
+  return function (tree) {
+    for (let i = 0; i < tree.children.length; i++) {
+      const node = tree.children[i];
+      if (node.type === 'paragraph') {
+        for (let j = 0; j < node.children.length; j++) {
+          const child = node.children[j];
+          if (
+            child.type === 'link' &&
+            child.url.startsWith('http') &&
+            !child.url.includes('aria.ai')
+          ) {
+            child.data = child.data || {};
+            child.data.hProperties = child.data.hProperties || {};
+            child.data.hProperties.target = '_blank';
+            child.data.hProperties.rel = 'noreferrer';
+          }
+        }
+      }
+    }
+  };
+}
